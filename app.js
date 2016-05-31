4124bcb203ba52bcc7b8b36fd6fd62c4d4ed8854
Kyle MacDonald <kyle@carrotcreative.com>
2016-05-31 10:16:11 -0400

    add investments

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const cssnext = require('postcss-cssnext')
 const rucksack = require('rucksack-css')
 const sugarss = require('sugarss')
 const lost = require('lost')
+const svg = require('postcss-inline-svg')
 
 module.exports = {
   postcss: {
-    plugins: [cssnext, rucksack, lost],
+    plugins: [cssnext, rucksack, lost, svg],
     parser: sugarss
   },
   babel: { presets: ['es2015', 'stage-2'] },
-  locals: { foo: 'bar' },
-  ignore: ['**/layout.jade', '**/_*', '**/.*']
+  locals: { investments: require('./investments.json') },
+  ignore: ['**/layout.jade', '**/_*', '**/.*', '**/investments.json']
 }
